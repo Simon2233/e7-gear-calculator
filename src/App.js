26b5bbd7f806d007025124cda635f8323189128c
Simon2233 <simon.fu.1997@gmail.com>
2020-12-06 16:55:12 -0500

    boilerplate calculator
    basic addition

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Button from '@material-ui/core/Button';
+import PercentCalc from './components/percentCalc'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Ayoooo</h1>
-        <Button variant="contained" color="primary">
-          Hello World
-        </Button>
+        <h1>E7 Gear Calculator</h1>
+        <PercentCalc/>
       </header>
     </div>
   );
