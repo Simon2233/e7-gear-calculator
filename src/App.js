8edb8b04d3afbf8543b4d26da528b1bb6a6bbfde
Ken Huang <ktkhuang@hotmail.com>
2020-12-06 14:36:53 -0500

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ayoooo</h1>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
       </header>
     </div>
   );
