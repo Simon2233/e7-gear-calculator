f6af4ece852f599b83f751b63fc525fc2308d51d
Ken Huang <ktkhuang@hotmail.com>
2020-12-06 18:40:08 -0500

    UI Changes, Add Health

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import './App.css';
 import CharacterSelector from './components/CharacterSelector.js'
 import Button from '@material-ui/core/Button';
 import PercentCalc from './components/percentCalc'
+import Typography from '@material-ui/core/Typography';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-        <h1>E7 Gear Calculator</h1>
+        <h1>E7 Kermit Score Calculator</h1>
         <PercentCalc/>
         <CharacterSelector />
+        <Typography variant="h3">*If gear score is 60 or above, Kermit recommends reforging <strong>right away</strong>!!!</Typography>
       </header>
     </div>
   );
